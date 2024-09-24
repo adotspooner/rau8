f3c24e1838c94e728b87292a82da3c17866a8a98
Arthur Spooner <151565419+adotspooner@users.noreply.github.com>
2024-09-24 18:38:28 +0200

    feat: nfc normalize, free rust cstring

--- include/rau8.h
+++ include/rau8.h
@@ -7,3 +7,6 @@ bool rau8_valid(const char* str);
 int64_t rau8_bytes(const char* str);
 int64_t rau8_scalar_values(const char* str);
 int64_t rau8_grapheme_clusters(const char* str);
+
+char* rau8_normalize_nfc(const char* str);
+void rau8_free(char* str);
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,10 @@
 use std::ffi::CStr;
+use std::ffi::CString;
 use std::os::raw::c_char;
+use std::ptr;
 
 use unicode_segmentation::UnicodeSegmentation;
+use unicode_normalization::UnicodeNormalization;
 
 #[no_mangle]
 pub extern "C" fn rau8_valid(str: *const c_char) -> bool {
@@ -47,4 +50,25 @@ pub extern "C" fn rau8_grapheme_clusters(str: *const c_char) -> i64 {
     }
 
     return -1;
+}
+
+#[no_mangle]
+pub extern "C" fn rau8_normalize_nfc(str: *const c_char) -> *mut c_char {
+    let cstr = unsafe { CStr::from_ptr(str) };
+    
+    if let Ok(valid_utf8_string) = String::from_utf8(cstr.to_bytes().to_vec()) {
+        let normalized_utf8_string = valid_utf8_string.nfc().collect::<String>();
+        if let Ok(valid_utf8_cstring) = CString::new(normalized_utf8_string) {
+            return valid_utf8_cstring.into_raw();
+        }
+    }
+
+    return ptr::null_mut();
+}
+
+#[no_mangle]
+pub extern "C" fn rau8_free(str: *mut c_char) {
+    unsafe { 
+        let _raw_string_to_free = CString::from_raw(str); 
+    };
 }
